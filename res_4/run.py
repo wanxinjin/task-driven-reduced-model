89768e960eecd26cc4b2b67b30d0e1f466df7e26
wanxinjin <wanxinjin@gmail.com>
2022-02-24 18:27:00 -0500

    update

--- res_4/run.py
+++ res_4/run.py
@@ -40,11 +40,11 @@ R = np.eye(n_control)
 # ============================= establish the lcs learner (automatically initialized)
 reduced_n_lam = 3
 lcs_learner = TD.LCS_learner_regression(n_state, n_control, n_lam=reduced_n_lam, stiffness=0)
-lcs_learner.val_lcp_theta = 1. * np.random.randn(lcs_learner.n_lcp_theta)
+lcs_learner.val_lcp_theta = 1 * true_lcp_theta + 0.0 * np.random.randn(true_lcp_theta.size)
 
 # ============================= initialize the optimizer
 optimizier = opt.Adam()
-optimizier.learning_rate = 1e-2
+optimizier.learning_rate = 1e-3
 
 # ============================= initialize an evaluator for the learned system
 mpc_horizon = 5
@@ -52,25 +52,27 @@ evaluator = TD.LCS_evaluation_MPC(lcs_learner)
 evaluator.setCostFunction(Q, R, QN)
 evaluator.initializeMPC(mpc_horizon)
 
-# # ============================= initialize a random control sequence for random initial condition
-#
-# init_state_batch = np.random.randn(traj_count, n_state)
-# control_traj_batch = TD.randomControlTraj(traj_count, control_horizon, n_control)
-
-# # =============================== compute the true optimal cost (for reference)
-# true_sys_oc_solver = TD.LCS_MPC(A, B, C, D, E, F, lcp_offset)
-# true_sys_oc_solver.oc_setup(control_horizon)
-# true_sys_opt_control_traj_batch = []
-# for i in range(traj_count):
-#     true_sys_sol = true_sys_oc_solver.mpc(init_state_batch[i], Q, R, QN)
-#     true_sys_opt_control_traj_batch += [true_sys_sol['control_traj_opt']]
-# true_sys_opt_state_traj_batch, true_sys_opt_lam_traj_batch = true_sys.sim_dyn(init_state_batch,
-#                                                                               true_sys_opt_control_traj_batch)
-# true_sys_opt_cost_batch = evaluator.computeCost(true_sys_opt_control_traj_batch, true_sys_opt_state_traj_batch)
+# # ============================= compute the true optimal cost (for reference, this is not always correct)
+traj_count = 30
+init_state_batch = np.random.randn(traj_count, n_state)
+state_batch_traj = [list(init_state_batch)]
+control_batch_traj = []
+for t in range(control_horizon):
+    # compute the control input using the current lcs learner
+    st = time.time()
+    control_batch_traj += [evaluator.mpc(true_sys, state_batch_traj[-1])]
+    # simulate to the next step
+    next_state_batch, lam_batch = true_sys.dyn_step(state_batch_traj[-1], control_batch_traj[-1])
+    state_batch_traj += [next_state_batch]
+# reorganize the data format
+control_traj_batch = TD.dataReorgnize(control_batch_traj)
+state_traj_batch = TD.dataReorgnize(state_batch_traj)
+true_sys_opt_cost_batch = evaluator.computeCost(control_traj_batch, state_traj_batch)
+print('| true_sys optimal cost (for reference):', np.mean(true_sys_opt_cost_batch))
 
 # ================= starting the data-driven model learning process
 control_cost_trace = []
-for control_iter in range(1):
+for control_iter in range(50):
 
     # # ============================= random the initial condition
     traj_count = 30
@@ -90,10 +92,25 @@ for control_iter in range(1):
     control_traj_batch = TD.dataReorgnize(control_batch_traj)
     state_traj_batch = TD.dataReorgnize(state_batch_traj)
 
-    # print
-    print(len(control_traj_batch))
-    print(len(state_traj_batch))
-    print(control_traj_batch[0].shape)
-    print(state_traj_batch[0].shape)
 
-    print('hahaha')
+    # ============================= learn the true lcs system from the true data
+    print('======================================================================')
+    print('| Control Iter:', control_iter)
+    if control_iter < 2:
+        lcs_max_iter = 2000
+    else:
+        lcs_max_iter = 500  # why: because the lcs_learner is always warm started with the previous control iteration
+    TD.LCSLearningRegression(lcs_learner, optimizier, control_traj_batch, state_traj_batch, max_iter=lcs_max_iter,
+                             print_level=1)
+
+
+    # ============================= print
+    true_sys_cost_batch = evaluator.computeCost(control_traj_batch, state_traj_batch)
+
+    print(
+        '\n|****** Control Iter:', control_iter,
+        '| true_sys current cost:', np.mean(true_sys_cost_batch),
+        '| true_sys optimal cost (for reference):', np.mean(true_sys_opt_cost_batch),
+        '\n'
+    )
+    control_cost_trace += [np.mean(true_sys_cost_batch)]
